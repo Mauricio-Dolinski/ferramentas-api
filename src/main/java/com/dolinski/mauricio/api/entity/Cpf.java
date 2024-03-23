1adc154318fe2598c357a5f2e0e8bbbf9efeba80
Mauricio Dolinski <mauriciompd@gmail.com>
2024-03-23 20:01:33 -0300

    Cpf refatorando

--- src/main/java/com/dolinski/mauricio/api/entity/Cpf.java
+++ src/main/java/com/dolinski/mauricio/api/entity/Cpf.java
@@ -1,33 +1,30 @@
 package com.dolinski.mauricio.api.entity;
 
 public class Cpf implements Documento{
+
+	private int digito;
+	private int[] soma = new int[2];
     
     @Override
     public String gerar() {
-        int[] digitos = new int[11];
-		int soma1 = 0;
-		int soma2 = 0;
-		String gerado = "";
+		digito = soma[0] = soma[1] = 0;
+		String cpf = "";
 
         for (int i = 0; i < 9; i++) {
-            digitos[i] = (int) (Math.random() * 9);
-			soma1 += digitos[i] * (i+1);
-			soma2 += digitos[i] * i;
+            digito = (int) (Math.random() * 9);
+			soma[0] += digito * (i+1);
+			soma[1] += digito * i;
+			cpf += "" + digito;
         }
 
-		digitos[9] = soma1 % 11;
-		if (digitos[9] == 10) digitos[9] = 0;
-
-		soma2 += digitos[9] * 9;
-
-		digitos[10] = soma2 % 11;
-		if (digitos[10] == 10) digitos[9] = 0;
-
-		for (int digito : digitos) {
-			gerado += "" + digito;
+		for (int i = 0; i < 2; i++) {
+			digito = soma[i] % 11;
+			if (digito == 10) digito = 0;
+			cpf += "" + digito;
+			if (i == 0) soma[1] += digito * 9;
 		}
  
-        return gerado;
+        return "CPF " + format(cpf) + " gerado.";
     }
 
     @Override
@@ -35,7 +32,8 @@ public class Cpf implements Documento{
         cpf = cpf.trim();
         cpf = cpf.replaceAll("\\.", "");
         cpf = cpf.replaceAll("-", "");
-        if (cpf.length() == 10 && cpf.matches("[0-9]+")){
+
+        if (cpf.length() == 10){
             cpf = "0" + cpf;
         }
         if (cpf.length() != 11 || !cpf.matches("[0-9]+")){
@@ -43,69 +41,42 @@ public class Cpf implements Documento{
         }
         else {
 
-            // Validação do CPF
-            int d1, d2;
-            int digito1, digito2, resto;
-            int digitoCPF;
-            String nDigResult;
-
-            d1 = d2 = 0;
-            digito1 = digito2 = resto = 0;
+			int[] digitoVerificador = new int[2];
+            int resto = 0;
+            String resultado;
+			String verificador = cpf.substring(cpf.length() - 2, cpf.length());;
+            digito = soma[0] = soma[1] = 0;
 
             for (int i = 1; i < cpf.length() - 1; i++) {
-                digitoCPF = Integer.valueOf(cpf.substring(i - 1, i)).intValue();
-
-                // multiplique a ultima casa por 2 a seguinte por 3 a seguinte por 4
-                // e assim por diante.
-                d1 = d1 + (11 - i) * digitoCPF;
-
-                // para o segundo digito repita o procedimento incluindo o primeiro
-                // digito calculado no passo anterior.
-                d2 = d2 + (12 - i) * digitoCPF;
+                digito = Integer.valueOf(cpf.substring(i - 1, i)).intValue();
+                soma[0] += digito * (11 - i);
+                soma[1] += digito * (12 - i);
             };
 
-            // Primeiro resto da divisão por 11.
-            resto = (d1 % 11);
+			for (int i = 0; i < 2; i++) {
+				resto = soma[i] % 11;
+            	if (resto <= 1)
+                	digitoVerificador[i] = 0;
+            	else
+                	digitoVerificador[i] = 11 - resto;
+				if (i == 0) soma[1] += 2 * digitoVerificador[0];
+			}
 
-            // Se o resultado for 0 ou 1 o digito é 0 caso contrário o digito é 11
-            // menos o resultado anterior.
-            if (resto < 2)
-                digito1 = 0;
-            else
-                digito1 = 11 - resto;
-
-            d2 += 2 * digito1;
-
-            // Segundo resto da divisão por 11.
-            resto = (d2 % 11);
-
-            // Se o resultado for 0 ou 1 o digito é 0 caso contrário o digito é 11
-            // menos o resultado anterior.
-            if (resto < 2)
-                digito2 = 0;
-            else
-                digito2 = 11 - resto;
-
-            // Digito verificador do CPF que está sendo validado.
-            String nDigVerific = cpf.substring(cpf.length() - 2, cpf.length());
-
-            // Concatenando o primeiro resto com o segundo.
-            nDigResult = String.valueOf(digito1) + String.valueOf(digito2);
-
-            // comparar o digito verificador do cpf com o primeiro resto + o segundo
-            // resto.
-            if (nDigVerific.equals(nDigResult)){
-                cpf = cpf.substring(0, 3) + "." + cpf.substring(3, 6) + "." + cpf.substring(6, 9) + "-" + cpf.substring(9);
+            resultado = String.valueOf(digitoVerificador[0]) + String.valueOf(digitoVerificador[1]);
+            if (verificador.equals(resultado)){
+                return "CPF " + format(cpf) + " é válido.";
             }
             else{
                 return "CPF não é válido";
             }
         }
-        cpf = cpf.substring(0, 3) + "." + 
-              cpf.substring(4, 7) + "." +
-              cpf.substring(8, 11) + "-" +
-              cpf.substring(12);
-              
-        return "CPF " + cpf + " é válido";
     }
+
+	private String format(String cpf){
+		cpf = cpf.substring(0, 3) + "." + 
+               cpf.substring(3, 6) + "." +
+               cpf.substring(6, 9) + "-" +
+               cpf.substring(9);
+		return cpf;
+	}
 }
--- src/test/java/com/dolinski/mauricio/api/resource/CpfResourceTest.java
+++ src/test/java/com/dolinski/mauricio/api/resource/CpfResourceTest.java
@@ -14,7 +14,7 @@ public class CpfResourceTest {
     @Test
     public void deveriaGerarCpf() {
         given()
-                .when().get("/cpf")
+                .when().get("/cpf/gerar")
                 .then()
                 .statusCode(200)
                 .assertThat().body(containsString("gerado"));
@@ -23,20 +23,70 @@ public class CpfResourceTest {
     @Test
     public void deveriaValidarCpf() {
         given()
-                .param("cpf", "848.509.150-78")
-                .when().post("/cpf")
+                .param("cpf", "123.456.789-09")
+                .when().post("/cpf/validar")
                 .then()
                 .statusCode(200)
-                .assertThat().body(containsString("CPF é válido"));
+                .assertThat().body(containsString("CPF 123.456.789-09 é válido"));
+    }
+
+    @Test
+    public void deveriaValidarCpf2() {
+        given()
+                .param("cpf", "12345678909")
+                .when().post("/cpf/validar")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString("CPF 123.456.789-09 é válido"));
+    }
+
+    @Test
+    public void deveriaValidarCpf3() {
+        given()
+                .param("cpf", "18.457.127-81")
+                .when().post("/cpf/validar")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString("CPF 018.457.127-81 é válido"));
     }
 
     @Test
     public void naoDeveriaValidarCpf() {
         given()
                 .param("cpf", "10987654321")
-                .when().post("/cpf")
+                .when().post("/cpf/validar")
                 .then()
                 .statusCode(200)
                 .assertThat().body(containsString("CPF não é válido"));
+    }
+
+    @Test
+    public void naoDeveriaValidarCpf2() {
+        given()
+                .param("cpf", "10987654321231231")
+                .when().post("/cpf/validar")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString("CPF não é válido, deve conter 11 numeros"));
+    }
+
+    @Test
+    public void naoDeveriaValidarCpf3() {
+        given()
+                .param("cpf", "231231")
+                .when().post("/cpf/validar")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString("CPF não é válido, deve conter 11 numeros"));
+    }
+
+    @Test
+    public void naoDeveriaValidarCpf4() {
+        given()
+                .param("cpf", "abcdefghijk")
+                .when().post("/cpf/validar")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString("CPF não é válido, deve conter 11 numeros"));
     }
 }
