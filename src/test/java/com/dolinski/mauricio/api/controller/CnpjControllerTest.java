6d824b6af7038f9bcf861ccdfc0b094f737408ef
Mauricio Dolinski <mauriciompd@gmail.com>
2024-05-16 12:44:35 -0300

    Added more cnpj tests

--- src/test/java/com/dolinski/mauricio/api/controller/CnpjControllerTest.java
+++ src/test/java/com/dolinski/mauricio/api/controller/CnpjControllerTest.java
@@ -23,10 +23,60 @@ public class CnpjControllerTest {
     @Test
     public void deveriaValidarCnpj() {
         given()
-                .param("cnpj", "72.379.775/0001-20")
+                .param("cnpj", "01.234.567/8912-51")
                 .when().post("/cnpj")
                 .then()
                 .statusCode(200)
                 .assertThat().body(containsString("válido"));
+    }
+
+    @Test
+    public void deveriaValidarCnpj2() {
+        given()
+                .param("cnpj", "01234567891251")
+                .when().post("/cnpj")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString("válido"));
+    }
+
+    @Test
+    public void naoDeveriaValidarCnpj() {
+        given()
+                .param("cnpj", "01.234.567/8912-34")
+                .when().post("/cnpj")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString("CNPJ não é válido, digito verificador deveria ser "));
+    }
+
+    @Test
+    public void naoDeveriaValidarCnpj2() {
+        given()
+                .param("cnpj", "01.234.567/8912-345")
+                .when().post("/cnpj")
+                .then()
+                .statusCode(400)
+                .assertThat().body(containsString("CNPJ não é válido, deve conter 14 numeros."));
+    }
+
+    @Test
+    public void naoDeveriaValidarCnpj3() {
+        given()
+                .param("cnpj", "")
+                .when().post("/cnpj")
+                .then()
+                .statusCode(400)
+                .assertThat().body(containsString("Campo cnpj não pode ser vazio"));
+    }
+
+    @Test
+    public void naoDeveriaValidarCnpj4() {
+        given()
+                .param("cnpj", "abcdefghijklmn")
+                .when().post("/cnpj")
+                .then()
+                .statusCode(400)
+                .assertThat().body(containsString("CNPJ não é válido, deve conter 14 numeros."));
     }
 } 
