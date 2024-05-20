65096c1fe5b484cd6250a4d23a9b5b6cb53e9108
Mauricio Dolinski <mauriciompd@gmail.com>
2024-05-20 11:57:34 -0300

    Added cnh validation

--- src/main/java/com/dolinski/mauricio/api/service/CnhService.java
+++ src/main/java/com/dolinski/mauricio/api/service/CnhService.java
@@ -5,6 +5,8 @@ import org.jboss.resteasy.reactive.RestResponse.ResponseBuilder;
 
 import com.dolinski.mauricio.api.dto.DocumentoDTO;
 
+import jakarta.validation.ValidationException;
+
 public class CnhService implements DocumentoService {
 
     @Override
@@ -14,6 +16,61 @@ public class CnhService implements DocumentoService {
 
     @Override
     public RestResponse<String> validar(DocumentoDTO dto) {
-        return ResponseBuilder.ok("valido").build();
+
+        try {
+            dto.parse();
+        } catch (ValidationException e) {
+            return ResponseBuilder.create(RestResponse.Status.BAD_REQUEST, e.getMessage()).build();
+        }
+
+        String cnh = dto.getNumero();
+
+        String response = "";
+        int[] soma = somarDigitos(cnh);
+        String digitosVerificadores = gerarDigitosVerificadores(soma);
+        
+
+        if (cnh.substring(9).equals(digitosVerificadores)){
+            response = "CNH " + cnh + " é válido.";
+        }
+        else {
+            if (digitosVerificadores.contains("-")){
+                response = "CNH não é válido.";
+            }
+            else response = "CNH não é válido, digito verificador deveria ser " + digitosVerificadores + ".";
+        }
+        
+        return ResponseBuilder.ok(response).build();
+    }
+
+    private int[] somarDigitos(String cnh){
+        int digito = 0;
+        int peso = 9;
+        int[] soma = new int[]{0, 0};
+
+        for (int i = 0; i < 9; i++, peso--) {
+            digito = Character.getNumericValue(cnh.charAt(i));
+            soma[0] += digito * peso;
+            soma[1] += digito * (i+1);
+        }
+        
+        return soma;
+    }
+
+    private String gerarDigitosVerificadores(int[] soma){
+        String digitosVerificadores = "";
+        int digito, sobra = 0;
+        
+        for (int i = 0; i < 2; i++) {
+            digito = soma[i] % 11;
+            if (digito >= 10) {
+                digito = 0;
+                if (sobra == 0) sobra = 2;
+            }
+            else digito = (digito - sobra);
+            digitosVerificadores += "" + digito;
+        }
+
+        return digitosVerificadores;
     }
 } 
--- src/test/java/com/dolinski/mauricio/api/controller/CnhControllerTest.java
+++ src/test/java/com/dolinski/mauricio/api/controller/CnhControllerTest.java
@@ -23,10 +23,70 @@ public class CnhControllerTest {
     @Test
     public void deveriaValidarCnh() {
         given()
-                .param("cnh", "123")
+                .param("cnh", "12345678900")
                 .when().post("/cnh")
                 .then()
                 .statusCode(200)
-                .assertThat().body(containsString("valido"));
+                .assertThat().body(containsString(" é válido."));
+    }
+
+    @Test
+    public void deveriaValidarCnh2() {
+        given()
+                .param("cnh", "93012679304")
+                .when().post("/cnh")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString(" é válido."));
+    }
+
+    @Test
+    public void deveriaValidarCnh3() {
+        given()
+                .param("cnh", "61274364119")
+                .when().post("/cnh")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString(" é válido."));
+    }
+
+    @Test
+    public void deveriaValidarCnh4() {
+        given()
+                .param("cnh", "77175131760")
+                .when().post("/cnh")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString(" é válido."));
+    }
+
+    @Test
+    public void deveriaValidarCnh5() {
+        given()
+                .param("cnh", "32142598597")
+                .when().post("/cnh")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString(" é válido."));
+    }
+
+    @Test
+    public void deveriaValidarCnh6() {
+        given()
+                .param("cnh", "98765432100")
+                .when().post("/cnh")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString(" é válido."));
+    }
+
+    @Test
+    public void naoDeveriaValidarCnh6() {
+        given()
+                .param("cnh", "98765432100")
+                .when().post("/cnh")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString("CNH não é válido."));
     }
 } 
