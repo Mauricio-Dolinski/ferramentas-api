fbb73cc172e0f688802700172a45878dab8f2ce8
Mauricio Dolinski <mauriciompd@gmail.com>
2024-09-21 14:02:08 -0300

    Added Cpf Controller Tests

--- src/test/java/com/dolinski/mauricio/api/controller/CpfControllerTest.java
+++ src/test/java/com/dolinski/mauricio/api/controller/CpfControllerTest.java
@@ -3,6 +3,7 @@ package com.dolinski.mauricio.api.controller;
 import io.quarkus.test.junit.QuarkusTest;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.matchesPattern;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,7 @@ import static io.restassured.RestAssured.*;
 @QuarkusTest
 public class CpfControllerTest {
 
+    
     @Test
     public void deveriaGerarCpf() {
         given()
@@ -21,9 +23,28 @@ public class CpfControllerTest {
     }
 
     @Test
+    public void deveriaGerarCpfComFormatoValido() {
+        given()
+                .when().get("/cpf")
+                .then()
+                .statusCode(200)
+                .assertThat().body(matchesPattern("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}"));
+    }
+
+    @Test
     public void deveriaValidarCpf() {
         given()
                 .param("cpf", "123.456.789-09")
+                .when().post("/cpf")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString("CPF 123.456.789-09 é válido."));
+    }
+
+    @Test
+    public void deveriaValidarCpfComEspacos() {
+        given()
+                .param("cpf", " 123.456.789-09 ")
                 .when().post("/cpf")
                 .then()
                 .statusCode(200)
@@ -51,7 +72,7 @@ public class CpfControllerTest {
     }
 
     @Test
-    public void naoDeveriaValidarCpf() {
+    public void naoDeveriaValidarCpfComDigitoVerificadorErrado() {
         given()
                 .param("cpf", "12345678910")
                 .when().post("/cpf")
@@ -61,7 +82,7 @@ public class CpfControllerTest {
     }
 
     @Test
-    public void naoDeveriaValidarCpf2() {
+    public void naoDeveriaValidarCpfComMaisDeOnzeNumeros() {
         given()
                 .param("cpf", "1098765432123456")
                 .when().post("/cpf")
@@ -71,7 +92,7 @@ public class CpfControllerTest {
     }
 
     @Test
-    public void naoDeveriaValidarCpf3() {
+    public void naoDeveriaValidarCpfComMenosDeOnzeNumeros() {
         given()
                 .param("cpf", "123456")
                 .when().post("/cpf")
@@ -88,5 +109,43 @@ public class CpfControllerTest {
                 .then()
                 .statusCode(400)
                 .assertThat().body(containsString("CPF não é válido, deve conter 11 numeros."));
+    }
+
+    @Test
+    public void naoDeveriaValidarCpfComLetras() {
+        given()
+                .param("cpf", "123.456.78A-09")
+                .when().post("/cpf")
+                .then()
+                .statusCode(400)
+                .assertThat().body(containsString("CPF não é válido, deve conter 11 numeros."));
+    }
+
+    @Test
+    public void naoDeveriaValidarCpfComCaracteresEspeciais() {
+        given()
+                .param("cpf", "123.456.789-0@")
+                .when().post("/cpf")
+                .then()
+                .statusCode(400)
+                .assertThat().body(containsString("CPF não é válido, deve conter 11 numeros."));
+    }
+
+    @Test
+    public void naoDeveriaValidarCpfComTodosNumerosIguais() {
+        given()
+                .param("cpf", "111.111.111-11")
+                .when().post("/cpf")
+                .then()
+                .statusCode(200)
+                .assertThat().body(containsString("CPF não é válido"));
+    }
+
+    @Test
+    public void deveriaLidarComRequisicaoSemParametroCpf() {
+        given()
+                .when().post("/cpf")
+                .then()
+                .statusCode(400);
     }
 }
